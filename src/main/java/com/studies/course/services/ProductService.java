05a063c6de37d6b976b2979d4c14a0f576213053
Gabrielly Freire <gabylopesfreire@gmail.com>
2024-01-31 19:01:06 -0300

    feat: Product

---
+++ src/main/java/com/studies/course/services/ProductService.java
@@ -0,0 +1,25 @@
+package com.studies.course.services;
+
+import com.studies.course.entities.Product;
+import com.studies.course.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    @Autowired
+    ProductRepository productRepository;
+
+    public List<Product> findAll(){
+        List list = productRepository.findAll();
+        return list;
+    }
+
+    public Product findById(Long id){
+        Optional<Product> product = productRepository.findById(id);
+        return product.get();
+    }
+}
